03bac02db70936a715f0b9c39424581a49a7c4b1
apl064 <apl064@gmail.com>
2022-05-24 22:29:37 +0300

    Initial Commit

---
+++ Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Pavel Metelin on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
